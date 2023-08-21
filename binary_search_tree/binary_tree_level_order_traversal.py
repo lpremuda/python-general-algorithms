d054bc8bef0ed01f90c02a18669bece3436c4259
Lucas Premuda <lucas.premuda@procore.com>
2023-08-21 16:32:03 -0700

    Binary Search Tree - medium - binary_tree_level_order_traversal.py

---
+++ binary_search_tree/binary_tree_level_order_traversal.py
@@ -0,0 +1,34 @@
+'''
+102. BINARY TREE LEVEL ORDER TRAVERSAL
+https://leetcode.com/problems/binary-tree-level-order-traversal
+'''
+
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> list[list[int]]:
+        if not root:
+            return []
+
+        queue = [root]
+        res = []
+
+        while queue:
+            level = []
+            for i in range(len(queue)):
+                node = queue.pop(0)
+                if node:
+                    level.append(node.val)
+                    queue.append(node.left)
+                    queue.append(node.right)
+            if level:
+                res.append(level)
+        
+        return res
